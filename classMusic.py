25c3b9174f3eea163b56c13b3937a71af3b02c82
nbelgada <44986498+nbelgada@users.noreply.github.com>
2023-10-16 13:37:00 +0200

    Added GroupingMusic Classes

--- classMusic.py
+++ classMusic.py
@@ -54,3 +54,58 @@ class Music:
         self.artists = value
     
         
+"""
+Mother class that is inherited,
+it specifies the base information
+that an aggregation of musics should
+have.
+"""
+class MusicGrouping :
+    def __init__(self,date):
+        self.musicList = []
+        self.name = ""
+        self.date = date
+
+    def getMusicList(self):
+        return self.musicList
+    def addToMusicList(self, music):
+        self.musicList.append(music)
+
+    def getName(self):
+        return self.name
+    def setName(self, newName):
+        self.name = newName
+
+    def getDate(self):
+        return self.date
+
+
+"""
+List of Musics that a user can create and modify
+at his own will.
+"""
+class PlayList(MusicGrouping) :#TO DO allow user to remove music
+    def __init__(self,date):
+        MusicGrouping.__init__(self,date)
+        self.modificationDate = MusicGrouping.getDate(self)
+
+    def getModificationDate(self):
+        return self.modificationDate
+    def setModificationDate(self, newDate):
+        self.modificationDate = newDate
+
+"""
+Album released by singers/Producers, once released
+it cannot be musics cannot be changed.
+"""
+class Album(MusicGrouping) :
+    def __init__(self,date):
+        MusicGrouping.__init__(self,date)
+        self.singerName
+        self.producerName
+
+    def getSingernName(self):
+        return self.singerName
+    def getProducerName(self):
+        return self.producerName
+
