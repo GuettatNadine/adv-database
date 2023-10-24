4fa27abf51861185cd21d7a72beb440b4d0253bd
nbelgada <naooufal.belgada@ulb.be>
2023-10-25 01:15:39 +0200

    random data generation + simple Select query + modification of the classes.

--- ZODBMusicClasses.py
+++ ZODBMusicClasses.py
@@ -4,36 +4,39 @@ import ZODB
 import ZODB.FileStorage
 import transaction
 import BTrees.OOBTree
+import faker
+import numpy as np
+from faker_music import MusicProvider
 
 
-class User(persistent.Persistent):
-    def __init__(self, userName, adress, email, age, gender, playlists):
+class User(persistent.Persistent):  # Make Object unique (add id and track the id)
+    def __init__(self, userName, address, email, age, gender, userId, playlistIds=persistent.list.PersistentList()):
         self.userName = userName
-        self.adress = adress
+        self.address = address
         self.email = email
         self.age = age
         self.gender = gender
-        self.playlists = playlists
-
-    def __eq__(self, other):
-        return self.userName == other.userName
-
-    def __gt__(self, other):
-        return self.age > other.age
-
-    def __lt__(self, other):
-        return self.age < other.age
-
-    def __ne__(self, other):
-        return self.userName != other.userName
+        self.playlistIds = playlistIds
+        self.userId = userId
 
     # getters
+    def __eq__(self, other):
+        return self.userId == other
+
+    def __lt__(self, other):
+        return self.userId < other
+
+    def __gt__(self, other):
+        return self.userId > other
+
+    def getUserId(self):
+        return self.userId
 
     def getUserName(self):
         return self.userName
 
-    def getAdress(self):
-        return self.adress
+    def getAddress(self):
+        return self.address
 
     def getEmail(self):
         return self.email
@@ -52,8 +55,8 @@ class User(persistent.Persistent):
     def setUserName(self, newUserName):
         self.userName = newUserName
 
-    def setAdress(self, newAdress):
-        self.adress = newAdress
+    def setAddress(self, newAddress):
+        self.address = newAddress
 
     def setEmail(self, newEmail):
         self.email = newEmail
@@ -64,12 +67,18 @@ class User(persistent.Persistent):
     def setGender(self, newGender):
         self.gender = newGender
 
-    def setPlayLists(self, newPlayLists):
-        self.playlists = newPlayLists
+    def setPlaylistIds(self, newPlayListIds):
+        self.playlistIds = newPlayListIds
+
+    def addPlaylistId(self, newPlayListId):
+        self.playlistIds.append(newPlayListId)
+
+    def removePlaylistId(self, playlistId):
+        self.playlistIds.remove(playlistId)
 
 
 class Music(persistent.Persistent):
-    def __init__(self, title, genre, duration, isVideo, writer, producer, artists):
+    def __init__(self, title, genre, duration, isVideo, writer, producer, artists, albumId):
         self.title = title
         self.genre = genre
         self.duration = duration
@@ -77,6 +86,7 @@ class Music(persistent.Persistent):
         self.writer = writer
         self.producer = producer
         self.artists = artists
+        self.albumId = albumId
 
     # getters
     def getTitle(self):
@@ -99,6 +109,9 @@ class Music(persistent.Persistent):
 
     def getArtists(self):
         return self.artists
+
+    def getAlbumId(self):
+        return self.albumId
 
     # setters
     def setTitle(self, value):
@@ -129,25 +142,41 @@ have.
 
 
 class MusicGrouping(persistent.Persistent):
-    def __init__(self, date):
-        self.musicList = persistent.list.PersistentList()
-        self.name = ""
+    def __init__(self, musicGroupingId, date, name):
+        self.musicGroupingId = musicGroupingId
+        self.name = name
         self.date = date
-
-    def getMusicList(self):
-        return self.musicList
-
-    def addToMusicList(self, music):
-        self.musicList.append(music)
-
-    def getName(self):
-        return self.name
+        self.musicIds = persistent.list.PersistentList()
 
     def setName(self, newName):
         self.name = newName
 
+    def getMusicGroupingId(self):
+        return self.musicGroupingId
+
+    def getName(self):
+        return self.name
+
     def getDate(self):
         return self.date
+
+    def getMusics(self):
+        return self.musicIds
+
+    def removeMusic(self, musicId):
+        self.musicIds.remove(musicId)
+
+    def removeMusics(self):
+        self.musicIds = persistent.list.PersistentList()
+
+    def setMusicIds(self, musicIdList):
+        self.musicIds = musicIdList
+
+    def addMusic(self, musicId):
+        self.musicIds.append(musicId)
+
+    def isMusicInGrouping(self, musicId):
+        return musicId in self.musicIds
 
 
 """
@@ -157,8 +186,8 @@ at his own will.
 
 
 class PlayList(MusicGrouping):  # TO DO allow user to remove music
-    def __init__(self, date):
-        MusicGrouping.__init__(self, date)
+    def __init__(self, musicGroupingId, date, name):
+        MusicGrouping.__init__(self, musicGroupingId, date, name)
         self.modificationDate = MusicGrouping.getDate(self)
 
     def getModificationDate(self):
@@ -175,10 +204,10 @@ it cannot be musics cannot be changed.
 
 
 class Album(MusicGrouping):
-    def __init__(self, date):
-        MusicGrouping.__init__(self, date)
-        self.singerName = ""
-        self.producerName = ""
+    def __init__(self, musicGroupingId, date, name, singerName, producerName):
+        MusicGrouping.__init__(self, musicGroupingId, date, name)
+        self.singerName = singerName
+        self.producerName = producerName
 
     def getSingerName(self):
         return self.singerName
@@ -193,33 +222,63 @@ def createZODB(fileName):
     return db
 
 
-def getTestUsers(n, tree):
-    for i in range(0, n):
-        print(i)
-        name = "HAHA" + str(i)
-        genre = "POP" + str(i)
-        duration = i
-        isVideo = (i % 2 == 0)
-        writer = "Jean" + str(i)
-        producer = "Frank" + str(i)
-        artists = "Paul" + str(i)
-        date = "22-10-2023"
-        userName = "User" + str(i)
-        adress = "Rue idk" + str(i)
-        email = userName + "@gmail.com"
-        age = 20 + i
-        gender = "M" if i % 2 == 0 else "F"
-
-        music = Music(name, genre, duration,
-                      isVideo, writer, producer, artists)
-        playList = PlayList(date)
-        user = User(userName, adress,
-                    email, age, gender, playList)
-        user.playlists.addToMusicList(music)
-        tree.insert(user, str(i))
+def getTestData(n, tree):
+    users = n // 2
+    playlistMusics = n // 4
+    albumMusics = n // 20
+    numberOfObjects = 0
+    fake = faker.Faker()
+    fake.add_provider(MusicProvider)
+    for albums in range(0, albumMusics):
+        gender = np.random.choice(["M", "F"], p=[0.5, 0.5])
+        artist = fake.first_name_male() if gender == "M" else fake.first_name_female()
+        title = fake.word()
+        producer = fake.name()
+        numberOfObjects += 1
+        album = Album(albums, fake.date(), fake.word(), artist, producer)
+        for musics in range(5):
+            numberOfObjects += 1
+            genre = fake.music_genre()
+            duration = np.random.randint(179, 250)
+            isVideo = np.random.randint(1)
+            writer = fake.name()
+            producer = fake.name()
+            tree["Musics"].insert((albums * 5) + musics, Music(title, genre,
+                                                               duration, isVideo, writer, producer, artist,
+                                                               (albums * 5) + musics))
+            album.addMusic((albums * 5) + musics)
+            transaction.commit()
+        tree["Albums"].insert(albums, album)
         transaction.commit()
-        # print(user.getUserName(), " ", user.getAge(), " ", user.getEmail()
-        #  , " ", user.getPlayLists().getMusicList())
+
+    for music in range(0, playlistMusics):
+        numberOfObjects += 1
+        playList = PlayList(music, fake.date(), fake.word())
+        for i in range(1, np.random.randint(1, 4)):
+            m = np.random.randint(albumMusics)
+            if not playList.isMusicInGrouping(m):
+                playList.addMusic(m)
+        tree["PlayLists"].insert(music, playList)
+        transaction.commit()
+
+    for userId in range(0, users):
+        numberOfObjects += 1
+        date = fake.date()
+        userName = fake.user_name()
+        address = fake.address
+        email = fake.email()
+        age = np.random.randint(17, 110)
+        gender = np.random.choice(["M", "F"], p=[0.5, 0.5])
+        playList = persistent.list.PersistentList()
+        for i in range(1, np.random.randint(1, 4)):
+            m = np.random.randint(playlistMusics)
+            if not (m in playList):
+                playList.append(m)
+        user = User(userName, address,
+                    email, age, gender, userId, playList)
+        tree["Users"].insert(userId, user)
+        transaction.commit()
+    return numberOfObjects
 
 
 def main():
@@ -230,20 +289,23 @@ def main():
         connection = db.open()
         root = connection.root()
         root['Users'] = BTrees.OOBTree.BTree()
-        getTestUsers(20, root['Users'])
+        root['Albums'] = BTrees.OOBTree.BTree()
+        root['PlayLists'] = BTrees.OOBTree.BTree()
+        root['Musics'] = BTrees.OOBTree.BTree()
+        n = input("approx. number of objects wanted :")
+        print(getTestData(int(n), root))
         connection.close()
     if choice == "load":
         storage = ZODB.FileStorage.FileStorage('MyZopeOODB.fs')
         db = ZODB.DB(storage)
         connection = db.open()
         root = connection.root()
-        usersTree = root["Users"]
+        print("Select userName From User where id = 485")
+        queryResults = list(root["Users"].values(485))
+        for queryResult in queryResults:
+            print(
+                "user : " + queryResult.getUserName() + " | " + "id : " + str(queryResult.getUserId()) + " stars")
         print("done")
-        for userKey in usersTree:
-            user = usersTree[userKey]
-            print(user, ': ', userKey.getUserName())
-            # print(user.getUserName(), " ", user.getAge(), " ", user.getEmail(),
-            #      " ", user.getPlayLists().getMusicList())
         connection.close()
     return 0
 
